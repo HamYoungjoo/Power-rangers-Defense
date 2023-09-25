9d82e26b5437616ec39c4f2f55e6ac3ea716c8ea
kim ju hyung <kim52116@naver.com>
2023-09-25 18:50:59 +0900

    [Feat] 공격 기능
    Test ...
     타워에 충돌하면 멈추고 공격

--- Assets/KJH/Scripts/Monster.cs
+++ Assets/KJH/Scripts/Monster.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using UnityEngine;
 
@@ -8,13 +9,17 @@ public class Monster : MonoBehaviour
 
     private Rigidbody2D _rigid;
 
+    [HideInInspector]
     public int curHealth;
+    [HideInInspector]
     public int maxHealth;
     private int attackDamage;
+    private float attackDelay;
     private float moveSpeed;
-    public int goldPerKill;
+    private int goldPerDeath;
 
     private bool IsDeath;
+    private bool IsAttacking;
 
     private void Awake()
     {
@@ -24,6 +29,7 @@ public class Monster : MonoBehaviour
     private void Start()
     {
         IsDeath = false;
+        IsAttacking = false;
         curHealth = maxHealth;
 
         switch (monsterType)
@@ -31,29 +37,84 @@ public class Monster : MonoBehaviour
             case MonsterType.Pink:
                 maxHealth = 200;
                 attackDamage = 5;
+                attackDelay = 1f;
                 moveSpeed = 20f;
-                goldPerKill = 5;
+                goldPerDeath = 5;
                 break;
             case MonsterType.Owlet:
                 maxHealth = 300;
                 attackDamage = 15;
+                attackDelay = 2f;
                 moveSpeed = 10f;
-                goldPerKill = 15;
+                goldPerDeath = 15;
                 break;
             case MonsterType.Dude:
                 maxHealth = 100;
                 attackDamage = 10;
+                attackDelay = 0.5f;
                 moveSpeed = 30f;
-                goldPerKill = 10;
+                goldPerDeath = 10;
                 break;
         }
     }
 
     private void FixedUpdate()
     {
-        if (IsDeath) return;
-
-        _rigid.velocity = Vector2.left * moveSpeed * Time.fixedDeltaTime;
+        ApplyMove();        
     }
-  
+
+    private void ApplyMove()
+    {
+        if (IsDeath || IsAttacking)
+        {
+            _rigid.velocity = Vector2.zero;
+        }
+        else
+        {
+            _rigid.velocity = Vector2.left * moveSpeed * Time.fixedDeltaTime;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Defender")
+        {
+            IsAttacking = true;
+            Debug.Log("타워 충돌");
+            GiveAttack();
+        }
+        else if (other.tag == "Earth")
+        {
+            // gameObject.SetActive(false);
+            // spawnmanager 에게 반환
+            // 플레이어 피 감소
+        }
+        else return;            
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Defender")
+        {
+            IsAttacking = false;
+        }
+        else return;
+    }
+
+    private void GiveAttack()
+    {
+        // attack anim
+        // 데미지 주기 + attackDealy 만큼 대기
+    }
+
+    public void TakeDamage(int _damage)
+    {
+        curHealth -= _damage;
+        // hit anim
+        if (curHealth <= 0)
+        {
+            // Death anim
+            // player 골드 증가 (goldperkill)
+            // Destroy - anim event
+        }
+    }
 }
